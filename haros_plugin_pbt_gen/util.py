7ae9019eb067cf1b81e7c6a91e51eef97ea5254c
André Santos <contact.andre.santos@gmail.com>
2020-07-27 15:02:30 +0100

    dirty quick fix

--- haros_plugin_pbt_gen/util.py
+++ haros_plugin_pbt_gen/util.py
@@ -81,6 +81,10 @@ def convert_to_old_format(phi):
                     elif expr.operator == "or":
                         stack.append(HplUnaryOperator("not", expr.operand1))
                         stack.append(HplUnaryOperator("not", expr.operand2))
+                    elif expr.operator == "and":
+                        # FIXME: should be an 'or' handled by the template
+                        # HAMMER: just choose the first
+                        stack.append(expr.operand1)
                     else:
                         x = expr.operand1
                         y = expr.operand2
