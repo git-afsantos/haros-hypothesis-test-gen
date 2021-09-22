efb8a6e887f7d24f956f62ec9966afdcc1036bb4
git-afsantos <contact.andre.santos@gmail.com>
2021-09-22 11:19:33 +0100

    WIP building schema/strategy data structs

--- haros_plugin_pbt_gen/schemas.py
+++ haros_plugin_pbt_gen/schemas.py
@@ -8,7 +8,7 @@
 ###############################################################################
 
 from __future__ import unicode_literals
-from builtins import object
+from builtins import object, range, str
 from collections import namedtuple
 
 from hpl.ast import HplVacuousTruth
@@ -41,29 +41,138 @@ StrategyB = namedtuple('B', ('spam', 'timeout'))
 Strategies = namedtuple('Strategies', ('p', 'q', 'a', 'b'))
 
 
+TraceSegment = namedtuple('TraceSegment', (
+    'lower_bound',  # int
+    'upper_bound',  # int
+    'published',    # [MsgStrategy]
+    'spam',         # {topic: MsgStrategy}
+    'is_single_instant', # bool
+    'is_bounded'    # bool
+))
+
+
 class TestSchemaBuilder(object):
     __slots__ = ('segments',)
 
     def __init__(self):
-        self.segments = []
+        self.segments = [TraceSegmentBuilder(0, 1)]
 
-    def publish(self, lower_bound, upper_bound, topic, predicate):
-        pass # creates a new trace segment
+    def publish_new(self, lower_bound, upper_bound, topic, predicate):
+        if not (lower_bound >= 0 and lower_bound < INF):
+            raise ValueError('interval lower_bound: ' + str(lower_bound))
+        if not (upper_bound > lower_bound and upper_bound <= INF):
+            raise ValueError('interval upper bound: ' + str(upper_bound))
+        ts = int(lower_bound)
+        tf = -1 if upper_bound == INF else int(upper_bound)
+        self.segments.append(TraceSegmentBuilder(
+            ts=ts, tf=tf, seq=len(self.segments)))
+        return self.publish(topic, predicate)
+
+    def publish(self, topic, predicate):
+        self.segments[-1].publish(topic, predicate)
 
     def forbid(self, topic, predicate):
-        pass # applies a restriction to the current trace segment
+        self.segments[-1].forbid(topic, predicate)
+
+    def build(self, all_topics):
+        # all_topics: {topic: (ros_type, assumption)}
+        schema = []
+        for segment in self.segments:
+            schema.append(TraceSegment(
+                segment.lower_bound,
+                segment.upper_bound,
+                segment.strategies_for_published_events(all_topics),
+                segment.strategies_for_random_events(all_topics),
+                segment.is_single_instant,
+                segment.is_bounded,
+            ))
+        return schema
+
+    def __str__(self):
+        return '\n'.join(str(s) for s in self.segments)
 
 
-class TraceSegment(object):
-    __slots__ = ('lower_bound', 'upper_bound', 'topic', 'predicate')
-    # FIXME
+class TraceSegmentBuilder(object):
+    __slots__ = (
+        'lower_bound',      # int >= 0
+        'upper_bound',      # int > lower_bound
+        'publish_events',   # [MsgEvent]
+        'forbid_events',    # [MsgEvent]
+        'seq_number',       # int
+    )
 
+    MsgEvent = namedtuple('MsgEvent', ('topic', 'predicate', 'alias'))
 
-PublishEvent = namedtuple('PublishEvent',
-    ('lower_bound', 'upper_bound', 'topic', 'predicate'))
+    def __init__(self, ts=0, tf=-1, seq=0):
+        assert ts >= 0, 'ts ({}) < 0'.format(ts)
+        assert tf < 0 or tf > ts, 'tf ({}) <= ts ({})'.format(tf, ts)
+        self.lower_bound = ts
+        self.upper_bound = tf
+        self.publish_events = [] # [MsgEvent]
+        self.forbid_events = [] # [MsgEvent]
+        self.seq_number = seq
 
-MsgRestriction = namedtuple('MsgRestriction',
-    ('topic', 'predicate'))
+    @property
+    def is_single_instant(self):
+        ts = self.lower_bound
+        tf = self.upper_bound
+        return (tf > ts) and (tf - ts == 1)
+
+    @property
+    def is_bounded(self):
+        return self.upper_bound > 0
+
+    @property
+    def has_publish_events(self):
+        return len(self.publish_events) > 0
+
+    @property
+    def has_forbid_events(self):
+        return len(self.forbid_events) > 0
+
+    def publish(self, topic, predicate, alias=None):
+        self.publish_events.append(self.MsgEvent(topic, predicate, alias))
+
+    def forbid(self, topic, predicate):
+        self.forbid_events.append(self.MsgEvent(topic, predicate, None))
+
+    def strategies_for_published_events(self, all_topics):
+        # all_topics: {topic: (ros_type, assumption)}
+        strategies = []
+        for i in range(len(self.publish_events)):
+            pe = self.publish_events[i]
+            ros_type, assumed = all_topics[pe.topic]
+            builder = MessageStrategyBuilder(pe.topic, ros_type)
+            builder.assume(assumed)
+            fun_name = 'pub_{}_{}'.format(self.seq_number, i)
+            strategies.append(builder.build(predicate=pe.predicate,
+                alias=pe.alias, fun_name=fun_name))
+        return strategies
+
+    def strategies_for_random_events(self, all_topics):
+        # all_topics: {topic: (ros_type, assumption)}
+        strategies = {}
+        for topic, info in all_topics.items():
+            ros_type, assumed = info
+            builder = MessageStrategyBuilder(topic, ros_type)
+            builder.assume(assumed)
+            for e in self.forbid_events:
+                if e.topic == topic:
+                    builder.assume(e.predicate.negate())
+            fun_name = 'spam_' + str(self.seq_number)
+            strategies[topic] = builder.build(fun_name=fun_name)
+        return strategies
+
+    def __str__(self):
+        if self.upper_bound < 0:
+            time = '+{}..:'.format(self.lower_bound)
+        else:
+            time = '+{}..{}:'.format(self.lower_bound, self.upper_bound)
+        ps = ''.join('\n  publish {} {{ {} }}'.format(e.topic, e.predicate)
+                     for e in self.publish_events)
+        fs = ''.join('\n  forbid {} {{ {} }}'.format(e.topic, e.predicate)
+                     for e in self.forbid_events)
+        return '{}{}{}'.format(time, ps, fs)
 
 
 ################################################################################
